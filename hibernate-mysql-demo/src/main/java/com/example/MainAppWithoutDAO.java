82a648ee4bdf300bcd9eab592d383c60368d756c
DevangJayswal <dnjayswal@gmail.com>
2021-06-12 17:06:25 -0400

    Hibernate demo with MySql without DAO

---
+++ hibernate-mysql-demo/src/main/java/com/example/MainAppWithoutDAO.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.example.entity.Book;
+
+public class MainAppWithoutDAO {
+
+	public static void main(String[] args) {
+		SessionFactory sessionFactory;
+		
+		// configures settings from hibernate.cfg.xml
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+		// creating SessionFactory
+		sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+		// code to save a book
+		Book book = new Book();
+		book.setTitle("Python");
+		book.setAuthor("France Melion");
+		book.setPrice(42.59f);
+
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+
+		session.save(book);
+
+		session.getTransaction().commit();
+		session.close();
+
+		sessionFactory.close();
+
+	}
+
+}
