9828897da6aa5140833f65662edd84c17dba19cb
Devang Jayswal <dnjayswal@gmail.com>
2021-10-14 17:28:24 -0400

    created comment for Function functional interface demo

--- src/main/java/com/example/java8/FunctionInterface.java
+++ src/main/java/com/example/java8/FunctionInterface.java
@@ -5,6 +5,7 @@ import java.util.function.Function;
 public class FunctionInterface {
     public static void main(String[] args) {
 
+        // first generic parameter is input type and second is return type
         Function<String, String> upperCase = s -> s.toUpperCase();
         System.out.println(upperCase.apply("Aishwarya"));
 
