9583bbf86cf2689dc45731a80c5256367aacaa2f
Devang Jayswal <devang.jayswal@adidas.com>
2021-12-25 16:36:08 -0500

    added test

---
+++ DemoProjects/maven-sonar-demo/src/test/java/com/example/product/dao/ProductDAO.java
@@ -0,0 +1,13 @@
+package com.example.product.dao;
+
+import com.example.product.dto.Product;
+
+public interface ProductDAO {
+	void create(Product product);
+
+	Product read(int id);
+
+	void update(Product product);
+
+	void delete(int id);
+}
