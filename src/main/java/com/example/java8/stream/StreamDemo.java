4c5f79cc74d7441b8f9c7840034b9ce9571e85d3
Devang Jayswal <devang.jayswal@adidas.com>
2022-01-04 09:54:51 -0500

    created demo

---
+++ src/main/java/com/example/java8/stream/StreamDemo.java
@@ -0,0 +1,30 @@
+package com.example.java8.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class StreamDemo {
+    public static void main(String[] args) {
+        String[] stringArray = {"a", "b", "c"};
+        List<String> stringList = Arrays.asList("x", "y", "z");
+
+        // enhanced for loop
+        for (String s : stringArray) {
+            System.out.println(s);
+        }
+
+        // forEach
+        stringList.forEach(System.out::println);
+        // forEach can be used only with collection
+        // you cannot use stringArray
+
+        // forEach (Behind the scene)
+        stringList.forEach(s -> System.out.println(s));
+
+
+    }
+    static void myForEach(List<String> source, Consumer action){
+
+    }
+}
