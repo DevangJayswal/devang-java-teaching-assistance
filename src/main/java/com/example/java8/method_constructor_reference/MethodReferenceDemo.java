09f7332e46104c4a16f0910b6650772c5417e557
Jayswal, Devang <Devang.Jayswal@adidas.com>
2022-02-23 16:04:39 -0500

    updated Method Reference Demo

--- src/main/java/com/example/java8/method_constructor_reference/MethodReferenceDemo.java
+++ src/main/java/com/example/java8/method_constructor_reference/MethodReferenceDemo.java
@@ -2,7 +2,7 @@ package com.example.java8.method_constructor_reference;
 
 // instead of lambda expression we can use method reference
 
-// Interef is a functional interface as it had only one method i.e. m1()
+// Interef is a functional interface as it has only one method i.e. m1()
 interface Interef {
     void m1();
 }
@@ -28,7 +28,7 @@ public class MethodReferenceDemo {
         // in order to implement m1() method of functional interface Interef
         // we can use lambda expression
         Interef i = () -> System.out.println("m1()");
-        i.m1();
+        i.m1(); // m1()
 
         // now let's say instead of implementing m1() from the scratch
         // you want to use another method as an implementation for m1()
@@ -36,16 +36,23 @@ public class MethodReferenceDemo {
         // for e.g. we have method m2()
         // you want to use m2() as an implementation for m1() then you can use
         // method reference
+        // note that m2() is a static method of class MethodReferenceDemo
         Interef i1 = MethodReferenceDemo::m2; // we do not use () at the end of methodName
-        i1.m1();
+        i1.m1(); // static m2() method reference
 
         MethodReferenceDemo m = new MethodReferenceDemo();
+        // here for an implementation of m1() of functional interface Interef,
+        // we are using m3() method of class MethodReferenceDemo for
+        // an implementation of m1()
+        // note that m3() is an instance method
+        // so we will have to create an object first
+        // m is an object of MethodReferenceDemo
         Interef i2 = m::m3;
         i2.m1();
 
         // In the lambda expression we are providing completely new implementation
         // but in the case of method reference we are not providing new implementation
-        // already existing method we are reusing for the implementation of functional interface
+        // already existing method we are reusing for the implementation of method of functional interface
 
         // advantage of using method reference is code re-usability
 
