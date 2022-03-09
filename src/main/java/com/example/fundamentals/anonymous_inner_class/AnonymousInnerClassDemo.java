dd32182890bfa1c0155074759f14cb64d4c0f3ab
DevangJayswal <dnjayswal@gmail.com>
2022-03-09 16:49:26 -0500

    Update AnonymousInnerClassDemo.java

--- src/main/java/com/example/fundamentals/anonymous_inner_class/AnonymousInnerClassDemo.java
+++ src/main/java/com/example/fundamentals/anonymous_inner_class/AnonymousInnerClassDemo.java
@@ -6,16 +6,36 @@ package com.example.fundamentals.anonymous_inner_class;
 * 3. defined inside arguments
 * */
 
+/*
+public class Popcorn {
+    public void taste(){
+        System.out.println("salty");
+    }
+}
+* */
+
+/*
+public class SubPopcorn extends Popcorn{
+    @Override
+    public void taste() {
+        System.out.println("spicy");
+    }
+}
+*/
+
+// Base Class -> Popcorn -> taste(){"salty"}
+// Child Class -> Subpopcorn extends Popcorn -> @O -> taste(){"spicy"}
+
 public class AnonymousInnerClassDemo {
     public static void main(String[] args) {
 
         Popcorn p = new Popcorn();
-        p.taste();
+        p.taste(); // salty
 
         // our Popcorn class has salty taste, but we wanted spicy taste,
         // so we extended Popcorn class and created SubPopcorn child class
         SubPopcorn s = new SubPopcorn();
-        s.taste();
+        s.taste(); // spicy
 
         // we can implement the same thing with Anonymous inner class
         Popcorn p1 = new Popcorn(){
@@ -24,10 +44,16 @@ public class AnonymousInnerClassDemo {
                 System.out.println("spicy");
             }
         };
+        
+        // behind the scene AnonymousInner Class got created and then it has extended Popcorn class
+        // and overridden taste() method and returned the object of this AnonymousInner Class
+        // so you can say 
+        // class AnonymousInner extends Popcorn { @override taste("spicy") }
+        // Popcorn p1 = new Anonymous();
 
         // we extended the Popcorn class without name and created its own implementation
         // this is called Anonymous Inner class
-        p1.taste();
+        p1.taste(); // salty
 
         // for each Anonymous class separate .class file will be generated
         // in this case, name will be `AnonymousInnerClass$1.class`
